cc5b6c28f0bc8a912f62a51a6aef30fd333c191a
Gabriel Grenier <gabriel.braun-grenier@hotmail.com>
2020-03-27 14:36:13 -0400

    Initial commit

---
+++ OpenAutoClick/Form1.Designer.cs
@@ -0,0 +1,174 @@
+﻿namespace OpenAutoClick
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable nécessaire au concepteur.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Nettoyage des ressources utilisées.
+        /// </summary>
+        /// <param name="disposing">true si les ressources managées doivent être supprimées ; sinon, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Code généré par le Concepteur Windows Form
+
+        /// <summary>
+        /// Méthode requise pour la prise en charge du concepteur - ne modifiez pas
+        /// le contenu de cette méthode avec l'éditeur de code.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.numMinTime = new System.Windows.Forms.NumericUpDown();
+            this.numMaxTime = new System.Windows.Forms.NumericUpDown();
+            this.labMinTime = new System.Windows.Forms.Label();
+            this.labMaxTime = new System.Windows.Forms.Label();
+            this.labTitle = new System.Windows.Forms.Label();
+            this.labAQP = new System.Windows.Forms.Label();
+            this.labW = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.numMinTime)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numMaxTime)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // numMinTime
+            // 
+            this.numMinTime.Location = new System.Drawing.Point(14, 130);
+            this.numMinTime.Maximum = new decimal(new int[] {
+            100000000,
+            0,
+            0,
+            0});
+            this.numMinTime.Name = "numMinTime";
+            this.numMinTime.Size = new System.Drawing.Size(120, 20);
+            this.numMinTime.TabIndex = 1;
+            this.numMinTime.ValueChanged += new System.EventHandler(this.numMinTime_ValueChanged);
+            // 
+            // numMaxTime
+            // 
+            this.numMaxTime.Location = new System.Drawing.Point(149, 130);
+            this.numMaxTime.Maximum = new decimal(new int[] {
+            1000000000,
+            0,
+            0,
+            0});
+            this.numMaxTime.Name = "numMaxTime";
+            this.numMaxTime.Size = new System.Drawing.Size(120, 20);
+            this.numMaxTime.TabIndex = 2;
+            this.numMaxTime.ValueChanged += new System.EventHandler(this.numMaxTime_ValueChanged);
+            // 
+            // labMinTime
+            // 
+            this.labMinTime.AutoSize = true;
+            this.labMinTime.Font = new System.Drawing.Font("RuneScape UF", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labMinTime.ForeColor = System.Drawing.Color.Yellow;
+            this.labMinTime.Location = new System.Drawing.Point(10, 106);
+            this.labMinTime.Name = "labMinTime";
+            this.labMinTime.Size = new System.Drawing.Size(113, 21);
+            this.labMinTime.TabIndex = 4;
+            this.labMinTime.Text = "Min Time (ms)";
+            // 
+            // labMaxTime
+            // 
+            this.labMaxTime.AutoSize = true;
+            this.labMaxTime.Font = new System.Drawing.Font("RuneScape UF", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labMaxTime.ForeColor = System.Drawing.Color.Yellow;
+            this.labMaxTime.Location = new System.Drawing.Point(145, 106);
+            this.labMaxTime.Name = "labMaxTime";
+            this.labMaxTime.Size = new System.Drawing.Size(118, 21);
+            this.labMaxTime.TabIndex = 5;
+            this.labMaxTime.Text = "Max Time (ms)";
+            // 
+            // labTitle
+            // 
+            this.labTitle.AutoSize = true;
+            this.labTitle.Font = new System.Drawing.Font("RuneScape UF", 48F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labTitle.ForeColor = System.Drawing.Color.Yellow;
+            this.labTitle.Location = new System.Drawing.Point(3, 9);
+            this.labTitle.Name = "labTitle";
+            this.labTitle.Size = new System.Drawing.Size(300, 64);
+            this.labTitle.TabIndex = 6;
+            this.labTitle.Text = "OpenClicker";
+            // 
+            // labAQP
+            // 
+            this.labAQP.AutoSize = true;
+            this.labAQP.Font = new System.Drawing.Font("RuneScape UF", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labAQP.ForeColor = System.Drawing.Color.Yellow;
+            this.labAQP.Location = new System.Drawing.Point(10, 241);
+            this.labAQP.Name = "labAQP";
+            this.labAQP.Size = new System.Drawing.Size(55, 21);
+            this.labAQP.TabIndex = 7;
+            this.labAQP.Text = "a  q  p";
+            // 
+            // labW
+            // 
+            this.labW.AutoSize = true;
+            this.labW.Font = new System.Drawing.Font("RuneScape UF", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labW.ForeColor = System.Drawing.Color.Yellow;
+            this.labW.Location = new System.Drawing.Point(34, 261);
+            this.labW.Name = "labW";
+            this.labW.Size = new System.Drawing.Size(24, 21);
+            this.labW.TabIndex = 8;
+            this.labW.Text = "W";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.InitialImage = ((System.Drawing.Image)(resources.GetObject("pictureBox1.InitialImage")));
+            this.pictureBox1.Location = new System.Drawing.Point(64, 156);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(235, 176);
+            this.pictureBox1.TabIndex = 9;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.ClientSize = new System.Drawing.Size(296, 291);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.labW);
+            this.Controls.Add(this.labAQP);
+            this.Controls.Add(this.labTitle);
+            this.Controls.Add(this.labMaxTime);
+            this.Controls.Add(this.labMinTime);
+            this.Controls.Add(this.numMaxTime);
+            this.Controls.Add(this.numMinTime);
+            this.HelpButton = true;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "OpenClicker";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.numMinTime)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numMaxTime)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.NumericUpDown numMinTime;
+        private System.Windows.Forms.NumericUpDown numMaxTime;
+        private System.Windows.Forms.Label labMinTime;
+        private System.Windows.Forms.Label labMaxTime;
+        private System.Windows.Forms.Label labTitle;
+        private System.Windows.Forms.Label labAQP;
+        private System.Windows.Forms.Label labW;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
---
+++ OpenAutoClick/Form1.cs
@@ -0,0 +1,143 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Runtime.InteropServices;
+using System.Threading;
+using System.IO;
+using System.Runtime.InteropServices;
+using System.Reflection;
+using System.Drawing;
+
+namespace OpenAutoClick
+{
+    public partial class Form1 : Form {
+        //keyboard hook
+        private LowLevelKeyboardListener _listener;
+
+        //Mouse actions
+        [DllImport("user32.dll", CharSet = CharSet.Auto, CallingConvention = CallingConvention.StdCall)]
+        public static extern void mouse_event(uint dwFlags, uint dx, uint dy, uint cButtons, uint dwExtraInfo);
+        private const int MOUSEEVENTF_LEFTDOWN = 0x02;
+        private const int MOUSEEVENTF_LEFTUP = 0x04;
+        private const int MOUSEEVENTF_RIGHTDOWN = 0x08;
+        private const int MOUSEEVENTF_RIGHTUP = 0x10;
+
+        //cursor
+        [DllImport("user32.dll")]
+        public static extern IntPtr LoadCursorFromFile(string filename);
+
+        //state of autoclick
+        private Boolean run;
+
+        //min time and max time
+        private int minTime;
+        private int maxTime;
+
+        //running thread
+        private Thread runningThread;
+
+        //getters
+        private int getMinTime() {
+            return this.minTime;
+        }
+        private int getMaxTime() {
+            return this.maxTime;
+        }
+        private Thread getRunningThread() {
+            return this.runningThread;
+        }
+
+        //setters
+        private void setMinTime(int minTime) {
+            this.minTime = minTime;
+        }
+        private void setMaxTime(int maxTime) {
+            this.maxTime = maxTime;
+        }
+        public void setRunningThread() {
+            runningThread = new Thread(() => {
+                while (true) {
+                    Random rnd = new Random();
+                    int sleepTime = rnd.Next(getMinTime(), getMaxTime());
+                    Thread.Sleep(sleepTime);
+
+                    //mouse click
+                    uint X = (uint)Cursor.Position.X;
+                    uint Y = (uint)Cursor.Position.Y;
+                    mouse_event(MOUSEEVENTF_LEFTDOWN | MOUSEEVENTF_LEFTUP, X, Y, 0, 0);
+                }
+            });
+        }
+
+        public Form1() {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e){
+            _listener = new LowLevelKeyboardListener();
+            _listener.OnKeyPressed += _listener_OnKeyPressed;
+            _listener.HookKeyboard();
+
+            //this.Cursor = new Cursor(Directory.GetCurrentDirectory() + "\\DScim.cur");
+
+            Cursor mycursor = new Cursor(Cursor.Current.Handle);
+            IntPtr colorcursorhandle = LoadCursorFromFile(Directory.GetCurrentDirectory() + "\\DScim.cur");
+            mycursor.GetType().InvokeMember("handle", BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Instance | BindingFlags.SetField, null, mycursor, new object[] { colorcursorhandle });
+            this.Cursor = mycursor;
+        }
+
+        void _listener_OnKeyPressed(object sender, KeyPressedArgs e) {
+            //MessageBox.Show(e.KeyPressed.ToString());
+
+            if (e.KeyPressed.ToString() == "Insert") {
+                if(runningThread == null){ 
+                    setRunningThread();
+                    runningThread.Start();
+                }
+                else if(runningThread!=null && !runningThread.IsAlive) {
+                    setRunningThread();
+                    runningThread.Start();
+                }
+            }
+
+            if (e.KeyPressed.ToString() == "Delete" && runningThread != null) {
+                runningThread.Abort();
+            }
+
+        }
+
+        private void btnStart_Click(object sender, EventArgs e){
+            setRunningThread();
+            runningThread.Start();
+        }
+
+
+        private void btnStop_Click(object sender, EventArgs e) {
+            runningThread.Abort();
+        }
+
+        private void numMinTime_ValueChanged(object sender, EventArgs e) {
+            setMinTime((int)numMinTime.Value);
+            numMaxTime.Minimum = getMinTime() + 5;
+        }
+
+        private void numMaxTime_ValueChanged(object sender, EventArgs e) {
+            if((int)numMaxTime.Value > (getMinTime()+5) && (int)numMaxTime.Value > getMaxTime()) {
+                setMaxTime((int)numMaxTime.Value);
+            }
+        }
+
+        private void Window_Closing(object sender, System.ComponentModel.CancelEventArgs e) {
+            _listener.UnHookKeyboard();
+            if (runningThread != null) {
+                runningThread.Abort();
+            }
+        }
+    }
+}
